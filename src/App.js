fe8b470384f0bd63d0c72f2f8ebf4851608c2a24
boris <boris@gratchev.com>
2023-12-11 20:07:21 +0100

    winning

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import React, { useState } from 'react';
 function App() {
   const columns = 7;
   const rows = 6;
+  const win = 4;
 
   const [turnPlayerOne, setTurnPlayerOne] = useState(true);
+  const [won, setWon] = useState(false);
 
   const [board, setBoard] = useState(() => {
     const initialBoard = Array.from({ length: columns }, () => Array(rows).fill(0));
@@ -28,16 +30,116 @@ function App() {
     updatedBoard[index] = column;
 
     setBoard(updatedBoard);
+    
+    if(checkWin() != 0){
+      setWon(true);
+      return
+    }
     setTurnPlayerOne(!turnPlayerOne);
   };
+
+  const setField = (column, row, value) => {
+    const updatedBoard = [...board];
+    updatedBoard[column][row] = value;
+    setBoard(updatedBoard);
+  }
+
+  const checkWin = () => {
+    let winningCords = [];
+    //check horizontal
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns - (win - 1); j++) {
+        for(let k = 0; k < win; k++){
+          if (board[j + k][i] !== board[j][i] || board[j][i] === 0) {
+            break;
+          }
+          if (k === win - 1) {
+            for(let l = 0; l < win; l++){
+              winningCords.push([j + l, i]);
+              setField(j + l, i, 3);
+            }
+            console.log("win: " + board[j][i]);
+            console.log(winningCords);
+            return board[j][i];
+          }
+        }
+      }
+    }
+
+    //check vertical
+    for (let i = 0; i < columns; i++) {
+      for (let j = 0; j < rows - (win - 1); j++) {
+        for(let k = 0; k < win; k++){
+          if (board[i][j + k] !== board[i][j] || board[i][j] === 0) {
+            break;
+          }
+          if (k === win - 1) {
+            for(let l = 0; l < win; l++){
+              winningCords.push([i, j + l]);
+              setField(i, j + l, 3);
+            }
+            console.log("win: " + board[i][j]);
+            return board[i][j];
+          }
+        }
+      }
+    }
+
+    //check diagonal
+    for (let i = 0; i < columns - (win - 1); i++) {
+      for (let j = 0; j < rows - (win - 1); j++) {
+        for(let k = 0; k < win; k++){
+          if (board[i + k][j + k] !== board[i][j] || board[i][j] === 0) {
+            break;
+          }
+          if (k === win - 1) {
+            for(let l = 0; l < win; l++){
+              winningCords.push([i + l, j + l]);
+              setField(i + l, j + l, 3);
+            }
+            console.log("win: " + board[i][j]);
+            return board[i][j];
+          }
+        }
+      }
+    }
+
+    //check diagonal
+    for (let i = 0; i < columns - (win - 1); i++) {
+      for (let j = rows - 1; j > win - 2; j--) {
+        for(let k = 0; k < win; k++){
+          if (board[i + k][j - k] !== board[i][j] || board[i][j] === 0) {
+            break;
+          }
+          if (k === win - 1) {
+            for(let l = 0; l < win; l++){
+              winningCords.push([i + l, j - l]);
+              setField(i + l, j - l, 3);
+            }
+            console.log("win: " + board[i][j]);
+            return board[i][j];
+          }
+        }
+      }
+    }
+    return 0
+  }
 
   return (
     <div className="App">
       <h1>Vier Gewinnt !!!</h1>
-      
+      <h2>
+        {won ?
+          (turnPlayerOne ? "Spieler 1 gewinnt" : "Spieler 2 gewinnt") : 
+          (turnPlayerOne ? "Spieler 1 ist am Zug" : "Spieler 2 ist am Zug")
+        }
+      </h2>
+
       {board.map((column, index) => (
         <div key={index} className="column">
-          <Button variant="contained" onClick={() => handleButtonClick(index)}>lol</Button>
+          <Button variant="contained" color={turnPlayerOne? "primary" : "secondary"}
+          disabled={won}
+          onClick={() => handleButtonClick(index)}>o</Button>
           {column.map((field, index) => (
             <Field key={index} content={field}/>
           ))}
--- src/field.jsx
+++ src/field.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react';
+import { Button } from '@mui/material';
+import CircleIcon from '@mui/icons-material/Circle';
 
 const Field = (props) => {
 
     return (
         <div>
-            <div>{props.content}</div>
+            <CircleIcon color={props.content == 0? "disabled": (
+                    props.content == 1? "primary": (
+                        props.content == 2? "secondary": "error"
+                    )
+                )
+            }/>
         </div>
     );
 };
