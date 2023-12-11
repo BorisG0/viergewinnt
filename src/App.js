669d7196ee017ce9bd5e4b0ca21cd9a329a49d01
boris <boris@gratchev.com>
2023-12-11 19:23:55 +0100

    two players

--- src/App.js
+++ src/App.js
@@ -8,13 +8,27 @@ function App() {
   const columns = 7;
   const rows = 6;
 
+  const [turnPlayerOne, setTurnPlayerOne] = useState(true);
+
   const [board, setBoard] = useState(() => {
     const initialBoard = Array.from({ length: columns }, () => Array(rows).fill(0));
     return initialBoard;
   });
 
-  const handleButtonClick = (column) => {
-    console.log(column);
+  const handleButtonClick = (index) => {
+    const column = board[index];
+    
+
+    const lastIndex = column.lastIndexOf(0);
+    if (lastIndex == -1) return
+
+    column[lastIndex] = turnPlayerOne ? 1 : 2;
+    // Update the board state with the modified column
+    const updatedBoard = [...board];
+    updatedBoard[index] = column;
+
+    setBoard(updatedBoard);
+    setTurnPlayerOne(!turnPlayerOne);
   };
 
   return (
@@ -23,7 +37,7 @@ function App() {
       
       {board.map((column, index) => (
         <div key={index} className="column">
-          <Button variant="contained" onClick={() => handleButtonClick(column)}>lol</Button>
+          <Button variant="contained" onClick={() => handleButtonClick(index)}>lol</Button>
           {column.map((field, index) => (
             <Field key={index} content={field}/>
           ))}
